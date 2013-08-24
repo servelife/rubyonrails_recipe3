228217060e68e2a18b6365a8b8edc96bd8a03f5c
Alan Ruth <alanruth@live.com>
2013-08-24 17:55:50 -0600

    example rails project built from recipe 1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Servelife2::Application.config.secret_key_base = 'da63349ea8af67960ce3e5c01b792b7f2cccd1e8b13aa9993cef3a8b71c3f31a627d13ba18b2dbdc1e36af76258509fc00a0abafbde12da063d32da8e7cfb4af'
